017ef90a54b863ab92713abc1648ca395b346d18
Lbarrerob <141870263+Lbarrerob@users.noreply.github.com>
2024-06-02 18:40:52 -0500

    Delete p1_la_galeria directory

--- p1_la_galeria/src/consola/Main.java
+++
@@ -1,9 +0,0 @@
-package consola;
-
-public class Main {
-
-    public static void main(String[] args) {
-        ConsolaPrincipal consola = new ConsolaPrincipal();
-        consola.mostrarMenuPrincipal();
-    }
-}
